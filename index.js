17e8fd0cf671af6fc6a88b9be0476636a2ccaefe
TOXIC <108856741+TO3IC@users.noreply.github.com>
2023-08-26 18:26:53 +0800

    ~

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Express from "express";
 import cors from "cors";
 var app = Express()
 
-app.use(cors({ origin: "https://test-api-sigma-two.vercel.app/" }))
+app.use(cors({ origin: "*" }))
 
 
 app.use(bodyParser.json())
@@ -11,6 +11,10 @@ app.use(bodyParser.urlencoded())
 
 app.get("/", (req, res) => {
     res.end("Hello world")
+})
+
+app.post("/api/post-something",(req,res)=>{
+    res.json(req.params)
 })
 
 app.listen(4000, () => {
