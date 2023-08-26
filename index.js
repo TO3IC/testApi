36bdbb619e49574a6d1e390cb961330613c8ac1a
TOXIC <108856741+TO3IC@users.noreply.github.com>
2023-08-26 18:03:40 +0800

    ~

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import bodyParser from "body-parser";
 import Express from "express";
+import cors from "cors";
 var app = Express()
+
+app.use(cors({ origin: "https://test-api-alpha.vercel.app/" }))
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.end()
 })
 
-app.post("/", (req, res)=>{
-    
+app.post("/", (req, res) => {
+
 })
 
-app.listen(4000,()=>{
+app.listen(4000, () => {
     console.log("API hosted on local host 4000")
 })
